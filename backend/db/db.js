56f4991573becf02cf848188bf28b71fda3619bb
JuanDiegoSic <diego23jdsc@gmail.com>
2021-08-04 15:36:35 -0500

    My firts proyect V1

---
+++ backend/db/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.BD_CONNECTION,{
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+        });
+        console.log("The Connection is sucefull : ON");
+    } catch (e) {
+        console.log("Error connection to MongoDB: ",e);
+        throw new Error("Error connection to MongoDB");
+        
+    }
+};
+
+module.exports= {dbConnection};
